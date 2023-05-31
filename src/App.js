efe636126914552257838d9955fe205913ffa004
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-31 09:41:05 -0300

    Cria array 'images' com as 4 imagens que serão utilizadas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import { motion } from 'framer-motion'
 
+import image1 from '../src/images/1.jpg'
+import image2 from '../src/images/2.jpg'
+import image3 from '../src/images/3.jpg'
+import image4 from '../src/images/4.jpg'
+
+const images = [image1, image2, image3, image4]
+
+
 function App() {
   return (
     <div className="App">
